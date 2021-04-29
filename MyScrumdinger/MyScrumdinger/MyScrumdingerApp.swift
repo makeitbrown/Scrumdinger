0ebfdba0b839c7e60383d3fa2d1330f4be49eeef
Michael Brown <brown.michael.n@gmail.com>
2021-04-29 12:56:46 -0600

    Downloaded Starter Project and Created My Own

---
+++ MyScrumdinger/MyScrumdinger/MyScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyScrumdingerApp.swift
+//  MyScrumdinger
+//
+//  Created by Michael Brown on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct MyScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
